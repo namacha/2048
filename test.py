0fa106bfb4c11aeb2364624f904a653ea24334ce
ayu <mac.ayu15@gmail.com>
2018-08-09 14:26:09 +0900

    done: basic method

--- test.py
+++ test.py
@@ -8,18 +8,33 @@ class TestBoard(unittest.TestCase):
     def setUp(self):
         self.seed = 0
         self.initial_grid = [
-            [None, None, None, None],
-            [None, None, None, None],
-            [None, None,    2, None],
-            [None, None, None, None],
+            [0, 0, 0, 0],
+            [0, 0, 0, 0],
+            [0, 0, 2, 0],
+            [0, 0, 0, 0],
         ]
 
         self.grid_1 = [
-            [None, 8, 16, 64],
-            [None, None, 4, 32],
-            [None, None, 2, 4],
-            [None, None, 4, None],
+            [0, 8, 16, 64],
+            [0, 0, 4, 32],
+            [0, 0, 2, 4],
+            [0, 0, 4, 0],
         ]
+
+        self.grid_2 = [
+            [0, 0, 8, 0],
+            [0, 2, 2, 2],
+            [8, 0, 8, 4],
+            [0, 0, 4, 4],
+        ]
+
+        self.grid_stalemate = [
+            [2, 4, 2, 4],
+            [4, 2, 4, 2],
+            [2, 4, 2, 4],
+            [4, 2, 4, 2],
+        ]
+ 
 
     def test_init_board(self):
         b = Board(self.seed)
@@ -54,23 +69,107 @@ class TestBoard(unittest.TestCase):
 
     def test_spawn_minimum_1(self):
         b = Board(self.seed)
-        b.grid = [[... for _ in range(b.col)] for _ in range(b.row)]
-        self.assertRaises(GameOver, b.spawn_minimum)
+        b.grid = self.grid_stalemate
+        b.spawn_minimum()
+        self.assertTrue(b.gameover)
 
-    def test_collapse_0(self):
+    def test_squash_0(self):
         b = Board()
 
-        arr = [None, 2, None, 4]
-        self.assertEqual(b.collapse(arr), [2, 4, None, None])
+        arr = [0, 2, 0, 4]
+        self.assertEqual(b.squash(arr), [2, 4, 0, 0])
 
         arr = [8, 8, 8, 8]
-        self.assertEqual(b.collapse(arr), [16, 16, None, None])
+        self.assertEqual(b.squash(arr), [16, 16, 0, 0])
 
         arr = [2, 2, 4, 4]
-        self.assertEqual(b.collapse(arr), [4, 8, None, None])
+        self.assertEqual(b.squash(arr), [4, 8, 0, 0])
 
         arr = [2, 4, 8, 4]
-        self.assertEqual(b.collapse(arr), [2, 4, 8, 4])
+        self.assertEqual(b.squash(arr), [2, 4, 8, 4])
+
+    def test_transpose(self):
+        b = Board()
+        b.grid = self.grid_1
+        expected = [
+            [0, 0, 0, 0],
+            [8, 0, 0, 0],
+            [16, 4, 2, 4],
+            [64, 32, 4, 0],
+        ]
+        self.assertEqual(b.grid, self.grid_1)
+        b.transpose()
+        self.assertEqual(b.grid, expected)
+
+    def test_reversed(self):
+        b = Board()
+        b.grid = self.grid_1
+        expected = [
+            [64, 16, 8, 0],
+            [32, 4, 0, 0],
+            [4, 2, 0, 0],
+            [0, 4, 0, 0],
+        ]
+        self.assertEqual(b.grid, self.grid_1)
+        b.reverse()
+        self.assertEqual(b.grid, expected)
+
+    def test_calc_score(self):
+        b = Board()
+        b.grid = self.grid_1
+        self.assertEqual(b.calc_score(), 134)
+
+    def test_check_stalemate(self):
+        b = Board()
+        self.assertFalse(b.check_stalemate())
+        b.grid = [
+            [2, 2, 2, 2],
+            [2, 2, 2, 0],
+            [2, 2, 2, 2],
+            [2, 2, 2, 2],
+        ]
+        self.assertFalse(b.check_stalemate())
+        b.grid[1][3] = 2
+        self.assertFalse(b.check_stalemate())
+        b.grid = [
+            [2, 4, 2, 4],
+            [2, 4, 2, 4],
+            [2, 4, 2, 4],
+            [2, 4, 2, 4],
+        ]
+        self.assertFalse(b.check_stalemate())
+        b.grid = [
+            [2, 4, 2, 4],
+            [4, 2, 4, 2],
+            [2, 4, 2, 4],
+            [4, 2, 4, 2],
+        ]
+        self.assertTrue(b.check_stalemate())
+
+
+    def test_left(self):
+        b = Board(self.seed)
+        b.left()
+        expected = [
+            [0, 0, 0, 0],
+            [2, 0, 0, 0],  # spawn
+            [2, 0, 0, 0],
+            [0, 0, 0, 0],
+        ]
+
+        self.assertEqual(b.grid, expected)
+
+        b = Board(self.seed)
+        b.grid = self.grid_2
+        b.left()
+        expected = [
+            [8, 0, 0, 0],
+            [4, 2, 0, 0],
+            [16, 4, 0, 2],  # spawn
+            [8, 0, 0, 0],
+        ]
+        self.assertEqual(b.grid, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
